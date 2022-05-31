a9177bc5cedde25bad882c7ca4b2eb26fd43841c
Juny824 <rksekrh10@naver.com>
2022-05-31 22:49:18 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create_post/', views.PostCreate.as_view()),
-    #path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
-    #path('<int:pk>/', views.single_post_page),
-    #path('', views.index),
 ]
